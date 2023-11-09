2e046b79ee8a57d3161fd358b3655b2f91eeabc8
huckleberri6 <j-devore.1@onu.edu>
2023-11-09 12:19:03 -0500

    findRowIndex complete

--- P11-1/solution.cpp
+++ P11-1/solution.cpp
@@ -1,1 +1,37 @@
 
+#include <iostream>
+#include <string>
+using namespace std;
+
+const int ROWS = 13;
+
+// function prototypes
+int findRowIndex(int thisRow);
+int findSeatIndex(char thisSeat);
+void displaySeatColumn(char seats[][ROWS], int column);
+
+int main()
+{
+	int row;
+	cout << "Which row would you like to sit it? ";
+	cin >> row;
+	row = findRowIndex(row);
+}
+
+int findRowIndex(int thisRow)
+{
+	if (thisRow > ROWS || thisRow < 1)
+		cout << "Error - the row you have entered is invalid.";
+	else
+		return thisRow - 1;
+}
+
+int findSeatIndex(char thisSeat)
+{
+
+}
+
+void displaySeatColumn(char seats[][ROWS], int column)
+{
+
+}
